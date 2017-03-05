3e13b6b531d190c24ed061b97972aa9285f7ad29
David Winn <q@qypea.com>
2017-03-05 15:17:27 -0800

    Hack things up so window size is detected and x is redrawn on resize

--- x1.cpp
+++ x1.cpp
@@ -23,28 +23,29 @@ int main(){
   XMapWindow( dsp, win );
 
 
-  long eventMask = StructureNotifyMask;
+  long eventMask = StructureNotifyMask|PropertyChangeMask;
   XSelectInput( dsp, win, eventMask );
 
   XEvent evt;
   do{
-   if (evt.type == MapNotify) {
+   if (evt.type == MapNotify || evt.type == ConfigureNotify) {
      GC gc = XCreateGC( dsp, win,
                         0,        // mask of values
                         NULL );   // array of values
      XSetForeground( dsp, gc, black );
 
+     if (evt.type == ConfigureNotify) {
+       width = evt.xconfigure.width;
+       height = evt.xconfigure.height;
+     }
 
      XDrawLine(dsp, win, gc, 10, 10, width - 10, height - 10); //from-to
      XDrawLine(dsp, win, gc, 10,height - 10,width - 10, 10); //from-to
 
-
-     eventMask = ButtonPressMask|ButtonReleaseMask;
-     XSelectInput(dsp,win,eventMask); // override prev
    }
 
     XNextEvent( dsp, &evt );   // calls XFlush()
-  }while( evt.type != ButtonRelease );
+  }while( evt.type != DestroyNotify );
 
   XDestroyWindow( dsp, win );
   XCloseDisplay( dsp );
