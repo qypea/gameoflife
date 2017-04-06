4e00bc14b933f1651dc22702cdb402bcb3c66fc3
David Winn <q@qypea.com>
2017-04-05 21:45:10 -0700

    Refactor to run as a game, redrawing at 30fps

--- gameoflife.cpp
+++ gameoflife.cpp
@@ -1,4 +1,5 @@
 #include <X11/Xlib.h> // preceede most other headers!
+#include <stdio.h>
 
 int main() {
    Display *dsp = XOpenDisplay(NULL);
@@ -23,50 +24,65 @@ int main() {
 
    int x11_fd = ConnectionNumber(dsp);
    fd_set in_fds;
+
    struct timeval tick_timer;
+   struct timeval fps;
+   fps.tv_usec = 1000000 / 30;
+   fps.tv_sec = 0;
+   tick_timer = fps;
 
    long eventMask = StructureNotifyMask | PropertyChangeMask;
    XSelectInput(dsp, win, eventMask);
 
    XEvent evt;
    while (1) {
-      if (evt.type == MapNotify || evt.type == ConfigureNotify)
-      {
-         GC gc = XCreateGC(dsp, win,
-                           0,        // mask of values
-                           NULL);   // array of values
-
-         if (evt.type == ConfigureNotify)
-         {
-            width = evt.xconfigure.width;
-            height = evt.xconfigure.height;
-         }
-
-         XSetBackground(dsp, gc, white);
-         XSetForeground(dsp, gc, black);
-         XDrawLine(dsp, win, gc, 10, 10, width - 10, height - 10); //from-to
-         XDrawLine(dsp, win, gc, 10, height - 10, width - 10, 10); //from-to
-      }
-
       FD_ZERO(&in_fds);
       FD_SET(x11_fd, &in_fds);
 
-      // TODO: Set timer such that we're capped at 30fps, not slower than 30fps
-      tick_timer.tv_usec = 1000000 / 30;
-      tick_timer.tv_sec = 0;
+      int stat = select(x11_fd + 1, &in_fds, NULL, NULL, &tick_timer);
+      // Note: If input driven then remaining time is in tick_timer
+      bool redraw = false;
 
-      // TODO: Check response code maybe?
-      select(x11_fd + 1, &in_fds, NULL, NULL, &tick_timer);
-
-      if (XPending(dsp))
+      // Deal with events
+      while (XPending(dsp))
       {
          XNextEvent(dsp, &evt);   // calls XFlush()
          if (evt.type == DestroyNotify)
          {
+            fprintf(stderr, "destroyed\n");
             break;
          }
+
+         if (evt.type == ConfigureNotify)
+         {
+            fprintf(stderr, "configured\n");
+            width = evt.xconfigure.width;
+            height = evt.xconfigure.height;
+            redraw = true;
+         }
+
+         if (evt.type == MapNotify)
+         {
+            fprintf(stderr, "mapped\n");
+            redraw = true;
+         }
       }
-   }
+
+      if (stat == 0 || redraw)
+      {
+         tick_timer = fps; // reset fps timer
+
+         GC gc = XCreateGC(dsp, win,
+                           0,        // mask of values
+                           NULL);    // array of values
+
+         XSetForeground(dsp, gc, white);
+         XFillRectangle(dsp, win, gc, 0, 0, width, height);
+         XSetForeground(dsp, gc, black);
+         XDrawLine(dsp, win, gc, 10, 10, width - 10, height - 10); //from-to
+         XDrawLine(dsp, win, gc, 10, height - 10, width - 10, 10); //from-to
+      }
+  }
 
    XDestroyWindow(dsp, win);
    XCloseDisplay(dsp);
